b3d2e0c59f5fe8eaf6b34b67bdf0951c17c11297
Mayank Baiswar <mayankb_03@hotmail.com>
2021-07-12 12:13:18 +0530

    Add files via upload

---
+++ .gitignore/src/useAnalytics.js
@@ -0,0 +1,154 @@
+import MetalClient, { BrowserMetricsPlugin, ResourceMetricsPlugin } from '@atlassiansox/metal-client';
+import AnalyticsWebClient, {
+    envType,
+    originType,
+    tenantType,
+    userType,
+    eventType,
+    platformType,
+} from '@atlassiansox/analytics-web-client';
+
+const ANALYTICS_NODE = '[data-analytics]';
+const OBSERVABILITY_NODE = '[data-observability]';
+
+export const GasEventTypes = {
+    SCREEN: 'screen',
+    TRACK: 'track',
+    UI: 'ui',
+    OPERATIONAL: 'operational',
+};
+
+export const AnalyticsConfig = {
+    [GasEventTypes.TRACK]: {
+        SUBJECT: {
+            CHANGE_PRICING_PLAN: 'changePricingPlan',
+        }
+    },
+    [GasEventTypes.UI]: {
+        ACTION: {
+            CLICKED: 'clicked',
+            SEARCHED: '`searched',
+            CHANGED: 'changed',
+        },
+        SUBJECT: {
+            BUTTON: 'button',
+            LINK: 'link',
+            NAV_ITEM: 'navItem',
+            TABLE_ROW: 'tableRow',
+            FORM: 'form',
+            FILTER: 'filter',
+        },
+        SUBJECTID: {
+            OPENSIDEBARLINK: 'openSidebarLink'
+        }
+    },
+    [GasEventTypes.SCREEN]: {
+
+    },
+    [GasEventTypes.OPERATIONAL]: {
+
+    }
+}
+
+const getAnalyticsConfig = (event, eventType) => {
+    if (!event.target) {
+        console.error('Not a valid HTML event target');
+        return;
+    }
+    let baseConfig = {
+        containers: {
+            project: {
+                id: 'css-xp',
+            },
+        }
+    }
+
+    switch (eventType) {
+        case GasEventTypes.SCREEN:
+            return {
+                name: 'main', // required
+                ...baseConfig
+            }
+        case GasEventTypes.TRACK:
+        case GasEventTypes.UI:
+            console.log(event);
+            const actionSubjectId = event.target.getAttribute('data-actionsubjectid');
+            const actionSubject = event.target.getAttribute('data-actionsubject');
+            const action = event.target.getAttribute('data-action');
+            if (!actionSubjectId || !actionSubject || !action) {
+                console.error(`Please make sure to set data-actionsubjectid, data-actionsubject, data-action on the HTML element for sending UI events`);
+                return;
+            }
+            return {
+                ...baseConfig,
+                actionSubjectId,
+                actionSubject,
+                action
+            }
+        case GasEventTypes.OPERATIONAL:
+    }
+}
+
+const useAnalytics = () => {
+    // initialise metal/gas/sentry
+
+    const sendObservability = () => {
+        const productInfo = {
+            metalId: 'your-metal-id-here',
+            version: '1.0.2',
+            env: envType.STAGING,
+        };
+
+        const plugins = [
+            new BrowserMetricsPlugin({ rootId: 'root' }),
+            // new ResourceMetricsPlugin(),
+        ];
+
+        new MetalClient({ productInfo, plugins });
+
+    }
+
+    const sendAnalytics = (source) => (event, eventType) => {
+        const analyticsClient = new AnalyticsWebClient({
+            env: envType.DEV, // required
+            product: 'css-xp', // required
+            version: '1.0.0',
+            origin: originType.DESKTOP, // defaults to WEB if not specified
+            platform: platformType.MAC, // defaults to WEB if not specified
+            locale: 'en-US',
+        });
+        analyticsClient.setTenantInfo(tenantType.NONE);
+        // analyticsClient.
+
+        let sendEvent;
+        switch (eventType) {
+            case GasEventTypes.UI:
+                sendEvent = analyticsClient.sendUIEvent;
+                break;
+            case GasEventTypes.SCREEN:
+                sendEvent = analyticsClient.sendScreenEvent;
+                break;
+            case GasEventTypes.TRACK:
+                sendEvent = analyticsClient.sendTrackEvent;
+                break;
+            case GasEventTypes.OPERATIONAL:
+                sendEvent = analyticsClient.sendOperationalEvent;
+                break;
+        }
+
+        try {
+            const config = getAnalyticsConfig(event, eventType);
+            sendEvent({ ...config, source });
+        } catch (err) {
+            console.error(err);
+        }
+    }
+
+    const sendErrors = () => {
+        // for sentry
+    }
+
+    return { sendObservability, sendAnalytics, sendErrors };
+}
+
+export default useAnalytics;
